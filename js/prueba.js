fcec00917929e046c1823e2b7d731fc01a57356a
martingomez092 <martingomez092@gmail.com>
2022-09-10 18:09:29 -0300

    Se agrega comprar multiple a travès de un carrito, y posibilidad de vaciar el carrito

---
+++ js/prueba.js
@@ -0,0 +1,12 @@
+const array = []
+
+const potus = {
+    nombre: "Potus",
+    id: 1,
+}
+
+const prompt1 = parseInt(prompt("Pone el primero"));
+const prompt2 = parseInt(prompt("Pone el segundo"));
+const prompt3 = parseInt(prompt("Pone el tercero"));
+
+prompt1 = array.push()
